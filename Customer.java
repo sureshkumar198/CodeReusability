d966ef9a2d3aa66030a28b58ca2d1ff9e6c23e9e
sureshkumar198 <suresh.shyamalas@gmail.com>
2022-10-11 23:48:26 +0100

    1

---
+++ Customer.java
@@ -0,0 +1,9 @@
+class Customer
+{
+
+public static void main (String[] args)
+{
+Mall mall = new Mall();
+mall.bookTicket(200);
+}
+}
---
+++ Mall.java
@@ -0,0 +1,8 @@
+class Mall
+
+{
+ void bookTicket(int amount)
+{
+    System.out.println("Balance is "+ (amount-120)); 
+    }
+}
